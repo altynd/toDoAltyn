5b8abd3df3ee3fb2ea2ddd5783cbc68918f828c9
Сэр Дъмитрий <altynd@mail.ru>
2019-02-10 21:52:34 +0300

    User persist using UserDefaults method

--- toDoAltyn/ToDoViewController.swift
+++ toDoAltyn/ToDoViewController.swift
@@ -9,40 +9,72 @@
 import UIKit
 
 class ToDoViewController: UITableViewController {
-
-    let itemArray = ["Сделай это","Потом сделай это","Потом сделай то"]
+    
+    let defaults = UserDefaults.standard
+    
+    var itemArray = ["Сделай это","Потом сделай это","Потом сделай то"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoItemCell") as? [String] {
+            itemArray = items
+        }
 
     }
-    
-// сколько строк будет в таблице
+    // сколько строк будет в таблице
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-//
+    //
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
         cell.textLabel?.text = itemArray[indexPath.row]
-
+        
         return cell
     }
     
-// выбор строки таблвью
+    // выбор строки таблвью
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         //проверка на галочку, если стоит то убрать
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else{
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add New Item", message: "massege", preferredStyle: .alert)
+        
+        var textField = UITextField()
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // что случится когда нажимаем кнопку +
+ 
+            self.itemArray.append(textField.text! )
+            
+            self.tableView.reloadData()
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion:  nil)
+    }
+    
+    
 }
 
